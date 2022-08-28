eb11fc817ef87922e60893beefa95e994eed25a7
Toheeb Olanrewaju Olagoke <43994117+Olacode01@users.noreply.github.com>
2022-08-28 21:16:31 +0000

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,26 @@
+#importing datetime from libraries 
+from datetime import datetime
+#importing playsound after downloading 
+import playsound
+alarm_time = input("Enter the time of alarm to be set:HH:MM:SS AM/PM\n")
+alarm_hour=alarm_time [0:2]
+alarm_minute=alarm_time[3:5]
+alarm_seconds=alarm_time[6:8]
+alarm_period=alarm_time[9:11].upper()
+print("Setting up alarn........")
+while True:
+    now=datetime.now()
+    current_hour=now.strftime("%I")
+    current_minute=now.strftime("%M")
+    current_seconds=now.strftime("%S")
+    current_period=now.strftime('%P')
+    if(alarm_period==current_period):
+        if (alarm_hour==current_hour):
+            if (alarm_minute==current_minute):
+                if (alarm_seconds==current_seconds):
+                    print ("wake up")
+                    playsound('desktop/python pro/alarm.mp3')
+                    break
+
+
+
